5fc9d2358335ed9cb8f457710fc0ed264fbd8235
Yeahya0510 <redgodyaya0510@gmail.com>
2021-04-04 04:04:44 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,26 @@
 import discord
 from discord.ext import commands
-import json
-import random, os, asyncio
 
 
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix='[', intents = intents)
+
+bot = commands.Bot(command_prefix= '[', intents = intents)
 
 @bot.event
 async def on_ready():
-   print(">> Bot is online <<")
+    print("> > Watson is Online~ < <")
 
 @bot.event
 async def on_member_join(member):
     channel = bot.get_channel(827844482983657482)
-    await channel.send(f'{member} join!')
+    await channel.send(f'{member} 進來了可晴的小學,一起歡迎他/她吧~')
     
 @bot.event
 async def on_member_remove(member):
     channel = bot.get_channel(827844482983657482)
-    await channel.send(f'{member} leave!')
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send(f'{round(bot.latency*1000)} (ms)')
+    await channel.send(f'{member} 從可晴的小學畢業了')
 
 
-
-bot.run('ODI3NTkzNTIzMjQyNzI5NDky.YGdShw.RwDaLuoBC1ZHeab983FLeSYYgXI')
+bot.run('ODI3NTkzNTIzMjQyNzI5NDky.YGdShw.YF2zNKpIUxR6MlzyhdX-M-ABrLY')
